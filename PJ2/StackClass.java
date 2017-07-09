a3d18deb68dd8578c3a8bc6433b3056f8e08f337
Brian Lai <brianlai30@outlook.com>
2017-07-08 22:30:00 -0700

    added abstract methods

--- PJ2/StackClass.java
+++ PJ2/StackClass.java
@@ -10,5 +10,35 @@ package PJ2;
  * @author Brian
  */
 public class StackClass<T> implements StackInterface {
+
+    @Override
+    public void push(Object newEntry) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object pop() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object peek() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int search(Object entry) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
