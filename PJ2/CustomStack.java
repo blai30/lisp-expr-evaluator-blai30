f0ac6f68cba0ae3ffa112d486efa965847ed09de
Brian Lai <brianlai30@outlook.com>
2017-07-08 22:15:11 -0700

    added comment description

--- PJ2/CustomStack.java
+++ PJ2/CustomStack.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * CSC220 Project 2 EC: Implementing a custom stack class.
  */
+
 package PJ2;
 
-/**
- *
- * @author Brian
- */
 public class CustomStack {
     
 }
